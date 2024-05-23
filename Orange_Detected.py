d9e8e7b93dc5ca2ed4273bc72305db2c811119e9
Shima Soleymanipour <155065873+shicoded@users.noreply.github.com>
2024-05-23 11:10:09 +0330

    Update and rename Detected_Orange.py to Orange_Detected.py

--- Orange_Detected.py
+++ Orange_Detected.py
@@ -2,10 +2,10 @@
 Recognet-Oranges-With-SVM
 Copyright (C) <2024/5/23> <neonicX-Tech>
 
-This library is free software; you can redistribute it and/or
+Orange_Detected is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+version 2.1 of the License, or any later version.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
